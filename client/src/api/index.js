03c5c3d56f6d5826dcf585d27a999865c8a72865
temelehtone <91484193+temelehtone@users.noreply.github.com>
2023-01-03 19:18:27 +0200

    change api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({ 
-  baseURL: process.env.PUBLIC_API_URL || "http://localhost:5000",
+  baseURL: "https://birdnest-fzvb.onrender.com",
   withCredentials: true
 });
 
